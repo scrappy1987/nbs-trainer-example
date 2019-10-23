28c3b3328509da287ac40e20ca923a814a9b42f3
scrappy1987 <john.gordon@qa.com>
2019-10-23 12:16:55 +0100

    Added rest component, entity and manually tested

--- src/main/java/com/qa/Application.java
+++ src/main/java/com/qa/Application.java
@@ -1,10 +1,13 @@
 package com.qa;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-
+		SpringApplication.run(Application.class, args);
 	}
 
 }
